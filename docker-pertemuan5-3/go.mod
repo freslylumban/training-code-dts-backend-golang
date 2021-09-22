9fadbf0d726b4f365d2722bb7477c578dd988ad9
freslylumban <freslylumban@gmail.com>
2021-09-22 07:59:29 +0700

    move all folder to grouping folder

--- docker-pertemuan5-3/go.mod
+++
@@ -1,3 +0,0 @@
-module docker-p6-3
-
-go 1.17
---
+++ docker/docker-pertemuan5-3/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:myexamplepassword@tcp(mysql-docker)/mysql")
+	if err != nil {
+		fmt.Println("Error DB:", err.Error())
+		return
+	}
+	defer db.Close()
+
+	fmt.Println("Connect to database.")
+
+	// name := os.Getenv("NAME")
+	// port := os.Getenv("PORT")
+
+	// http.HandleFunc("/", hello)
+
+	// http.ListenAndServe(fmt.Sprintf(":%v", port), nil)
+	// fmt.Println("Hello World, dari port", port, name)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
